ccae416c01cdf8f19c3868fba3140a1ae285ff1a
ferryyuwono <ferry@dihardjasoftware.com>
2021-03-28 13:58:56 +0700

    Create project 1.08.03 Solution Conditions

---
+++ 1.08.03-Solution-Conditions/app/src/main/java/com/study/android10803/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.study.android10803
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ 1.08.03-Solution-Conditions/app/src/test/java/com/study/android10803/ExampleUnitTest.kt
@@ -0,0 +1,51 @@
+package com.study.android10803
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun simplifiedIfVariableAssignment() {
+        //Given height is positive number
+        val height = 180
+
+        //Simplified variable assignment with if
+        val message = if (height < 90) {
+            println("Your height is under 90cm")
+            "You don't need to pay for MRT"
+        }
+        else {
+            println("Your height is equal or above 90cm")
+            "You need to pay for MRT"
+        }
+
+        //Print value of message
+        println(message)
+
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun simplifiedWhenVariableAssignment() {
+        //Given height is positive number
+        val userId = 1L
+
+        //Simplified variable assignment with if
+        val userRole = when (userId) {
+            1L -> "Admin"
+            2L, 4L, 5L -> "Supervisor"
+            3L, 6L, 7L -> "Finance"
+            else -> "User"
+        }
+
+        println("User Id: $userId has role: $userRole")
+
+        assertEquals(4, 2 + 2)
+    }
+}
