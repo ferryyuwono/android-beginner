478bf02e9ff40d0095da65a78d4d98c4d2422739
ferryyuwono <ferry@dihardjasoftware.com>
2021-03-29 22:46:08 +0700

    Add complexity

---
+++ 2.01.01-Demo-ProjectStructure/app/src/main/java/com/study/android20101/data/PersonData.kt
@@ -0,0 +1,3 @@
+package com.study.android20101.data
+
+data class PersonData(val firstName: String, val lastName: String, val age: Int)
---
+++ 2.01.01-Demo-ProjectStructure/app/src/main/java/com/study/android20101/ui/Main2Activity.kt
@@ -0,0 +1,13 @@
+package com.study.android20101.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.study.android20101.R
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
