4b5b4a897b1041a5f5903f6d074fb888bbbe38cb
ferryyuwono <ferry@dihardjasoftware.com>
2021-04-04 15:32:49 +0700

    Create project 2.04.02 Exercise ImageView ImageButton

---
+++ 2.04.02-Exercise-ImageViewImageButton/app/src/main/java/com/study/android20402/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.study.android20402
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //TODO: Create onClickCheckBoxBudiFavorite function
+    //TODO: FindViewById for checkBoxBudiFavorite, textViewBudiFavorite
+    //TODO: Using if condition, set textViewBudiFavorite.visibility to View.VISIBLE when checkBoxBudiFavorite is checked
+    //TODO: Else set textViewBudiFavorite.visibility to View.GONE
+
+
+    //TODO: Repeat for onClickCheckBoxAntonFavorite function
+}
